a0b1446ecfc04be812774625f4161f433d630ce4
Andrew Ke <andrew@andrewke.org>
2017-04-21 14:47:59 -0700

    Initial commit

---
+++ Task00.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Task00 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        System.out.println("The Force is strong with " + name);
+    }
+}
