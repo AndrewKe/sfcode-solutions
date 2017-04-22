264f561991ac2945f88f3077a018049d6c26db55
Nishan D'Souza <nishand@gmail.com>
2017-04-22 08:03:20 -0700

    Fixed Task03

--- Task03.java
+++ Task03.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.util.Collections;
 public class Task03
 {
     public static void main(String[] args)
@@ -18,20 +18,23 @@ public class Task03
                 votes[i]++;
                 rawVote = rawVote.substring(1);
             }
-
+            
             int winner = 0;
             for(int i : votes)
             {
                 if(i>winner) winner=i;
             }
-
-            String win = "";
-            if(votes[0] == winner) win+="Hoth ";
-            if(votes[1] == winner) win+="Jakku ";
-            if(votes[2] == winner) win+="Tatooine ";
-            if(votes[3] == winner) win+="Dagobah ";
-            if(votes[4] == winner) win+="Kashyyk";
-            System.out.println(win);
+            
+            ArrayList<String> winners = new ArrayList<>();
+            if(votes[0] == winner) winners.add("Hoth ");
+            if(votes[1] == winner) winners.add("Jakku ");
+            if(votes[2] == winner) winners.add("Tatooine ");
+            if(votes[3] == winner) winners.add("Dagobah ");
+            if(votes[4] == winner) winners.add("Kashyyk");
+            Collections.sort(winners);
+            
+            for(String win : winners) System.out.print(win);
+            System.out.println();
         }
     }
 }
