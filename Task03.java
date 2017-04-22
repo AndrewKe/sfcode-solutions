307486a6d7d5a2267f0e37eecca620dc6cba3f2a
Lalith Katta <lalzz@users.noreply.github.com>
2017-04-21 20:43:28 -0700

    Works now
    recoded from scratch

--- Task03.java
+++ Task03.java
@@ -1,46 +1,37 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Scanner;
 
-public class Task03 {
-    public static void main(String[] args) {
-        HashMap<Integer, String> planets = new HashMap<>();
-        planets.put(0, "Hoth");
-        planets.put(1, "Jakku");
-        planets.put(2, "Tatooine");
-        planets.put(3, "Dagobah");
-        planets.put(4, "Kashyyyk");
+public class Task3
+{
+    public static void main(String[] args)
+    {
+        Scanner read = new Scanner(System.in);
+        int t = read.nextInt();
+        read.nextLine();
+        for(int jkjk=0;jkjk<t;jkjk++)
+        {
+            String rawVote = read.nextLine();
+            int[] votes = {0,0,0,0,0};
+            while(rawVote.length()>0)
+            {
+                int i = Integer.parseInt(rawVote.substring(0,1));
+                votes[i]++;
+                rawVote = rawVote.substring(1);
+            }
 
-        Scanner in = new Scanner(System.in);
+            int winner = 0;
+            for(int i : votes)
+            {
+                if(i>winner) winner=i;
+            }
 
-        int rawVote = in.nextInt();
-        ArrayList<Integer> votes = new ArrayList<>();
-
-        /* Buggy Code Below */
-
-//        for (int i; rawVote > 0; i++) {
-//            i = rawVote % 10;
-//            votes.add(Integer.parseInt(rawVote.substring(i, i + 1)));
-//            rawVote /= 10;
-//        }
-
-        ArrayList<Integer> frequencies = new ArrayList<>();
-        ArrayList<Integer> winners = new ArrayList<>();
-
-        for (int j = 0; j < 5; j++) {
-            frequencies.add(Collections.frequency(votes, j));
+            String win = "";
+            if(votes[0] == winner) win+="Hoth ";
+            if(votes[1] == winner) win+="Jakku ";
+            if(votes[2] == winner) win+="Tatooine ";
+            if(votes[3] == winner) win+="Dagobah ";
+            if(votes[4] == winner) win+="Kashyyk";
+            System.out.println(win);
         }
-
-        int max = 0;
-        for (int i = 0; i < frequencies.size(); i++) {
-            if (frequencies.get(i) > max) {
-                winners.clear();
-                winners.add(i);
-                max = frequencies.get(i);
-            } else if (frequencies.get(i) == max) winners.add(i);
-        }
-
-        for (int i = 0; i < winners.size(); i++) System.out.print(planets.get(winners.get(i)) + " ");
     }
 }
